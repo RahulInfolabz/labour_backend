9d447761e27dbb7ad4f41d04db2b9abafcf54db6
rahulinfolabz <rahulinfolabz@gmail.com>
2026-03-26 11:30:19 +0530

    Upload Labour Backend Project

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "http://localhost:5174"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
@@ -98,7 +98,7 @@ app.post("/user/addFeedback", AddFeedback);
 app.get("/", (req, res) => {
   return res.status(200).json({
     status: true,
-    message: "Labout Backend Started."
+    message: "Labour Backend Started."
   })
 })
 
